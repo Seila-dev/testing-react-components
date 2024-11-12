982e71ee7df752b04c8a2e7cbc1876731c0d3759
Seila-Dev <erickrodrigues3975@gmail.com>
2024-11-11 22:21:32 -0300

    front-end quase feito, esperando backend

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -15,34 +15,34 @@ function Home() {
       name: 'Aline',
       age: 33,
       email: 'aline@gmail.com',
-      },
-]
+    },
+  ]
 
   return (
 
     <div className='container'>
-    <form>
-      <h1>Cadastro de Usuários</h1>
-      <input name='nome' type='text'/>
-      <input name='idade' type='number'/>
-      <input name='email' type='email'/>
-      <button type='button'>Cadastrar</button>
-    </form>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder='Nome' name='nome' type='text' />
+        <input placeholder='Idade' name='idade' type='number' />
+        <input placeholder='Email' name='email' type='email' />
+        <button type='button'>Cadastrar</button>
+      </form>
 
-    { users.map( user => (
-      <div key={user.id}>
-        <div>
-        <p>Nome: {user.name}</p>
-        <p>Idade: {user.age}</p>
-        <p>Email: {user.email}</p>
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
         </div>
-      <button>
-        <img src={Trash}/>
-      </button>
-    </div>
-        ))}  
+      ))}
 
-  </div>    
+    </div>
   )
 }
 
