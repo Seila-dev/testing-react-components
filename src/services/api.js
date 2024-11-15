af52edac70e220fc6ee85faaae71c133d2ca4e25
Seila-Dev <erickrodrigues3975@gmail.com>
2024-11-14 23:49:22 -0300

    Atualizações

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://testing-backend-8qik.onrender.com/'
+    baseURL: 'https://testing-backend-8qik.onrender.com'
 })
 
 export default api
